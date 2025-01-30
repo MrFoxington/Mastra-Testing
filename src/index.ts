3087d80ce62fa78339b1dedcfc108ca8235eab9d
John Fox <j.arcoudis@gmail.com>
2025-01-30 22:54:02 +1100

    Additional Quickstart Examples

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ const __dirname = dirname(__filename);
 
 // Load environment variables
 dotenv.config({
-    path: resolve(__dirname, '../.env.development')
+  path: resolve(__dirname, '../.env.development')
 });
 
-import { Mastra } from '@mastra/core';
+import { CoreMessage, Mastra } from '@mastra/core';
 import { createLogger } from '@mastra/core';
 import { weatherAgent } from './agents';
 
@@ -22,6 +22,40 @@ import { weatherAgent } from './agents';
 //   }),
 // });
 
+// // Basic Sample
+// const mastra = new Mastra();
+// const llm = mastra.LLM({
+//   provider: "ANTHROPIC",
+//   name: "claude-3-5-sonnet-20241022",
+//   apiKey: process.env.ANTHROPIC_API_KEY
+// })
+
+// const response = await llm.generate("Tell me about the moon landing")
+// console.log(response.text)
+
+
+// // System Prompted Message Sample
+// const mastra = new Mastra();
+// const llm = mastra.LLM({
+//   provider: "ANTHROPIC",
+//   name: "claude-3-5-sonnet-20241022",
+//   apiKey: process.env.ANTHROPIC_API_KEY
+// })
+
+// const message = [{
+//   role: "system",
+//   content: "You are Niel Armstrong"
+// },
+// {
+//   role: "user",
+//   content: "Tell me about the moon landing"
+// }] as CoreMessage[];
+
+// const response = await llm.generate(message)
+// console.log(response.text)
+
+
+// System Prompted Message - Streamed - Sample
 const mastra = new Mastra();
 const llm = mastra.LLM({
   provider: "ANTHROPIC",
@@ -29,6 +63,19 @@ const llm = mastra.LLM({
   apiKey: process.env.ANTHROPIC_API_KEY
 })
 
-const response = await llm.generate("Tell me about the moon landing")
-console.log(response.text)
+const message = [{
+  role: "system",
+  content: "You are Niel Armstrong"
+},
+{
+  role: "user",
+  content: "Retell the famouse moment of your moonlanding"
+}] as CoreMessage[];
 
+const stream = await llm.stream(message)
+
+console.log('STARTING STREAM READ \n')
+for await (const chunk of stream.textStream) {
+  process.stdout.write(chunk)
+}
+console.log('\n')
